2cff14cfaf268702b42d99b37c8bf84a0a77cdb3
abreuadrian <abreuadrianoliv@gmail.com>
2026-05-05 19:06:25 -0300

    add ex02

---
+++ aula10/exemplo10_pt1.py
@@ -0,0 +1,20 @@
+from utilities.help import time_clear
+
+#Ex01.: Tratamento de erro
+print(' ===== Cálculo de produtividade =====\n')
+while True: 
+    try:
+        total_produzido = float(input('Informe o valor total da venda: R$'))
+        funcionarios = int(input('Informe o total de funcionários: '))
+
+        media_funcionario = total_produzido / funcionarios
+
+        print(f'\nMédia por funcionário: R${media_funcionario:.2f}')
+        break
+    except ValueError: 
+            print('\nErro. Informe um valor válido.')
+            time_clear(1.5)
+    except ZeroDivisionError:
+            print('\nInforme um número de funcionários válido.')
+            time_clear(1.5)
+    
---
+++ aula10/exemplo10_pt2.py
@@ -0,0 +1,25 @@
+from utilities.help import time_clear
+#Ex02.: 
+# for i in range(5):
+#     total_produzido = float(input('Informe o valor total da venda: R$'))
+#     funcionarios = int(input('Informe o total de funcionários: '))
+#     media_funcionario = total_produzido / funcionarios
+
+#     print(f'\nMédia por funcionário: R${media_funcionario:.2f}')
+
+for i in range(5):
+    try:
+        total_produzido = float(input('Informe o valor total da venda: R$'))
+        funcionarios = int(input('Informe o total de funcionários: '))
+
+        media_funcionario = total_produzido / funcionarios
+
+        print(f'\nMédia por funcionário: R${media_funcionario:.2f}')
+        break
+    except ValueError: 
+            print('\nErro. Informe um valor válido.')
+            time_clear(1.5)
+    except ZeroDivisionError:
+            print('\nInforme um número de funcionários válido.')
+            time_clear(1.5)
+        
