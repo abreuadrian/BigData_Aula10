1fcd7a2fee2823b033f1ba578b176fd5fc723f11
abreuadrian <abreuadrianoliv@gmail.com>
2026-05-05 19:23:31 -0300

    add ex04

---
+++ aula10/exemplo10_pt4.py
@@ -0,0 +1,19 @@
+from utilities.helper import time_clear
+
+#Ex04.:
+try:
+    total_produzido = float(input('Informe o valor total da venda: R$'))
+    funcionarios = int(input('Informe o total de funcionários: '))
+    media_funcionario = total_produzido / funcionarios
+except ValueError: 
+        print('\nErro. Informe um valor válido.')
+        time_clear(1.5)
+except ZeroDivisionError:
+        print('\nInforme um número de funcionários válido.')
+        time_clear(1.5)
+else: 
+    print(f'\nMédia por funcionário: R${media_funcionario:.2f}')
+finally:  #Executa sempre. Com erro ou não.
+    time_clear(1.5)
+    print('Programa Encerrado')
+    
