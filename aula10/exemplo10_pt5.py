8c0ef5aed4869ce1c91a49c8b60db14774ed99a2
abreuadrian <abreuadrianoliv@gmail.com>
2026-05-05 20:24:31 -0300

    add ex05

---
+++ aula10/exemplo10_pt5.py
@@ -0,0 +1,16 @@
+#Ex05.:
+try:
+    total_produzido = float(input('Informe o valor total da venda: R$'))
+    funcionarios = int(input('Informe o total de funcionários: '))
+    media_funcionario = total_produzido / funcionarios
+
+except Exception as e:
+    print(f'Ops! ERRO do tipo: {e}.')
+except KeyboardInterrupt:
+    print('\nPrograma interrompido pelo usuário.')
+else: 
+    print(f'\nMédia por funcionário: R${media_funcionario:.2f}')
+    
+finally:  
+    print('Programa Encerrado')
+    
