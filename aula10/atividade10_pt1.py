423d1f41f8758e806081d41263474f9be078288e
Adrian Oliveira <abreuadrianoliv@gmail.com>
2026-05-06 10:53:16 -0300

    style: fix identation

--- aula10/atividade10_pt1.py
+++ aula10/atividade10_pt1.py
@@ -2,24 +2,24 @@
 saldo = 1000
 
 def saque():
-        while True:
-            try:
-                n = float(input('Informe o valor do saque: R$'))
-            except ValueError:
-                print('Erro. Insira um número válido. ')
-            else: 
-                return n
-
-def verify_saque(n):
-        if n > saldo:
-            print('Saldo insuficiente.')
-        elif n <= 0:
-            print('Digite um número maior que 0.')
+    while True:
+        try:
+            n = float(input('Informe o valor do saque: R$'))
+        except ValueError:
+            print('Erro. Insira um número válido. ')
         else: 
             return n
-            
+
+def verify_saque(n):
+    if n > saldo:
+        print('Saldo insuficiente.')
+    elif n <= 0:
+        print('Digite um número maior que 0.')
+    else: 
+        return n
+        
 def calc_total(saque):
-        return saldo - saque
+    return saldo - saque
 
 def display(saque, novo_saldo):
     print(f'\nFoi sacado R${saque:.2f} e restou R${novo_saldo:.2f} na conta')
